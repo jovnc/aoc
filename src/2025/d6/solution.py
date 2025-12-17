a94ce0e49644072930aadce6140a8733e1567d99
jovnc <95868357+jovnc@users.noreply.github.com>
2025-12-17 19:54:33 +0800

    [aoc] Implement 2025 Day 6 Solution Part 2

--- src/2025/d6/solution.py
+++ src/2025/d6/solution.py
@@ -3,16 +3,13 @@ from src.solution import Solution
 
 
 class Solution2025D6(Solution):
-    def parse_data(self, contents: str) -> Tuple[List[List[int]], List[str]]:
+    def parse_data(self, contents: str) -> str:
         """Parse the input data into a usable format."""
-        lines = contents.splitlines()
-        operators = [op for op in lines[-1].split(" ") if op]
-        numbers = [[int(num) for num in line.split(" ") if num] for line in lines[:-1]]
-        return numbers, operators
+        return contents
 
-    def solve_part_1(self, data: Tuple[List[List[int]], List[str]]) -> int:
+    def solve_part_1(self, data: str) -> int:
         """Solve part 1 of the problem."""
-        numbers, operators = data
+        numbers, operators = self._parse_helper_part_1(data)
         res = 0
         for col in range(len(operators)):
             op = operators[col]
@@ -28,4 +25,54 @@ class Solution2025D6(Solution):
 
     def solve_part_2(self, data: Any) -> Any:
         """Solve part 2 of the problem."""
-        pass
+        numbers, operators = self._parse_helper_part_2(data)
+        res = 0
+
+        for col in range(len(operators)):
+            op = operators[col]
+            colRes = numbers[col][0]
+            for row in range(1, len(numbers[col])):
+                if op == "+":
+                    colRes += numbers[col][row]
+                elif op == "*":
+                    colRes *= numbers[col][row]
+            res += colRes
+
+        return res
+
+    def _parse_helper_part_1(self, contents: str) -> Tuple[List[List[int]], List[str]]:
+        """Helper function to parse a line of integers."""
+        lines = contents.splitlines()
+        operators = [op for op in lines[-1].split(" ") if op]
+        numbers = [[int(num) for num in line.split(" ") if num] for line in lines[:-1]]
+        return numbers, operators
+
+    def _parse_helper_part_2(self, contents: str) -> Tuple[List[List[int]], List[str]]:
+        """Helper function to parse a line of integers."""
+        lines = contents.splitlines()
+        operator_line = lines[-1]
+        number_lines = lines[:-1]
+
+        numbers = []
+
+        col_starts = [idx for idx, c in enumerate(operator_line) if c != " "]
+        rows = len(number_lines)
+
+        # Get numbers
+        for start, end in zip(col_starts, col_starts[1:] + [len(number_lines[0]) + 1]):
+            curr = [
+                "".join(
+                    [
+                        number_lines[r][c]
+                        for r in range(rows)
+                        if number_lines[r][c] != " "
+                    ]
+                )
+                for c in range(start, end - 1)
+            ]
+            numbers.append([int(num) for num in curr if num])
+
+        # Get operators
+        operators = [op for op in operator_line.split(" ") if op]
+
+        return numbers, operators
