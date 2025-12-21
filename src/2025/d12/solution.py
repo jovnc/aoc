3be7e4b9e0bd5d7d87d23cb81346376bce58ea3d
jovnc <95868357+jovnc@users.noreply.github.com>
2025-12-21 15:26:26 +0800

    [aoc} Add template for 2025 Day 12

---
+++ src/2025/d12/solution.py
@@ -0,0 +1,16 @@
+from typing import Any
+from src.solution import Solution
+
+
+class Solution2025D12(Solution):
+    def parse_data(self, contents: str) -> Any:
+        """Parse the input data into a usable format."""
+        pass
+
+    def solve_part_1(self, data: Any) -> Any:
+        """Solve part 1 of the problem."""
+        pass
+
+    def solve_part_2(self, data: Any) -> Any:
+        """Solve part 2 of the problem."""
+        pass
